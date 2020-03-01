7a25c784ab60276356821178b08c20f72e9932ee
sissi <whmobil@gmail.com>
2020-03-01 10:13:49 +0100

    Bessere Erkennungsquote durch Bild-Optimierung

--- code/lib/ReadDigitalDigitClass.py
+++ code/lib/ReadDigitalDigitClass.py
@@ -77,9 +77,72 @@ class ReadDigitalDigit:
                 self.saveLogImage(image, value, logtime)
             self.result.append(value)
         return self.result
+    
+    def binarize_array(self, numpy_array, threshold=127):
+        for i in range(len(numpy_array)):
+            for j in range(len(numpy_array[0])):
+                if numpy_array[i][j] > threshold:
+                    numpy_array[i][j] = 255
+                else:
+                    numpy_array[i][j] = 0
+        return numpy_array
 
+    
+    
     def ReadoutSingleImage(self, image):
-        test_image = image.resize((20, 32), Image.NEAREST)
+        img = image
+        test_image = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        shape = test_image.shape
+        maxs = shape[0] * shape[1]
+        maxh = shape[0]
+        maxw = shape[1]
+        imgray = cv2.cvtColor(test_image, cv2.COLOR_BGR2GRAY)
+        imgray = cv2.bilateralFilter(imgray, 11, 17, 17)
+        edged = cv2.Canny(imgray, 250, 255)
+        contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        # print(contours)
+        contours_poly = [None] * len(contours)
+        boundRect = [None] * len(contours)
+        maxpos = -1
+        maxval = 0
+        x, y, w, h = 0, 0, 0, 0
+        for i, c in enumerate(contours):
+            contours_poly[i] = cv2.approxPolyDP(c, 2, True)
+            boundRect[i] = cv2.boundingRect(contours_poly[i])
+            size = (boundRect[i][2] * boundRect[i][3])
+            if size > maxval:
+                maxpos = i
+                maxval = size
+                x, y, w, h = boundRect[i]
+        x = x - 3
+        if x < 0:
+            x = 0
+        else:
+            w = w + 6
+            if w + x > maxw:
+                w = maxw - x
+        y = y - 3
+        if y < 0:
+            x = 0
+        else:
+            h = h + 6
+            if y + h > maxh:
+                h = maxh - y
+        xr = x + w
+        yu = y + h
+
+        img = np.asarray(test_image[y:yu, x:xr])
+        img = Image.fromarray(img)
+        img = img.convert("L")
+        img = img.filter(ImageFilter.GaussianBlur(radius=1))
+        img = np.array(img)
+        img = self.binarize_array(img)
+        img = Image.fromarray(img)
+        # img.show()
+        img = img.convert("RGB")
+        #        test_image = image.resize((20, 32), Image.NEAREST)
+        test_image = img.resize((20, 32), Image.NEAREST)
+        
         test_image.save('./image_tmp/resize.jpg', "JPEG")
         test_image = np.array(test_image, dtype="float32")
         img = np.reshape(test_image,[1,32,20,3])
